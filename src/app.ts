efd303981c0fc4de30b7ff45779a28135e6717ad
Anson Cheung <AnsonCheung@baby-kindom.com>
2022-02-27 16:10:27 +0800

    remove useless items

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import cors from 'cors';
 import * as express from 'express';
 import passport from 'passport';
-import path from 'path';
 import './utils/jwt.utils';
 import routes from './routes';
 
 const app = express.default();
 
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'pug');
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(express.json());
